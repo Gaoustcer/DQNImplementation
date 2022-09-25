358fdd3ad4ab4371d41747e7c49846a1a52b1ea8
Gaoustcer <2337284889@qq.com>
2022-09-25 11:36:20 +0800

    dueling net

--- dqnDueling.py
+++ dqnDueling.py
@@ -207,133 +207,47 @@ class DuelingDQN(DQNTarget_softupdate):
         self.optimizer = torch.optim.Adam(self.Advantagenet.parameters(),lr = self.lr)
         self.targetAdvantagenet = Advantagenet().cuda()
 
+    def interactwithenv(self, state=None):
+        if state is None:
+            state = self.train_env.reset()
+        action = torch.argmax(self.Advantagenet(state)).item()
+        return action,state
+    
+    def _softupdate(self):
+        # return super()._softupdate()
+        for targetparm,parm in zip(self.Advantagenet.parameters(),self.targetAdvantagenet.parameters()):
+            targetparm.copy_(
+                (1 - self.tau) * targetparm + self.tau * parm
+            )
+    def test(self):
+        reward = 0
+        for _ in range(self.testfreq):
+            done = False
+            state = self.test_env.reset()
+            while done == False:
+                action = torch.argmax(self.Advantagenet(state)).item()
+                ns,r,done,_ = self.test_env.step(action)
+                state = ns
+                reward += r
+        return reward/self.testfreq
+        # return super().interactwithenv(state)
     def _trainanepoch(self, update_freq=16):
         # return super()._trainanepoch(update_freq)
         for _ in range(update_freq):
-            
-    # def train(self):
-        # return super().train()
-
-        
-        
-# class DQNAgent:
-#     # import datetime
-#     def __init__(self,train_epoch=400,MAX_SIZE=512,device = torch.device('cuda:0')) -> None:
-#         self.trainenv = make_env()
-#         self.testenv = make_env()
-#         self.targetnet = Net(device=device)
-#         self.net = Net(device=device)
-#         self.device = device
-#         self.memory = np.random.random((MAX_SIZE,10))
-#         self.memoryindex = 0
-#         self.epsilon = 0.1
-#         self.maxsize = MAX_SIZE
-#         self.batchsize = 32
-#         self.optimizer = torch.optim.Adam(self.net.parameters(),lr=0.01)
-#         self.testtimes = 32
-#         self.trainepoch = train_epoch
-#         self.testlog = 0
-#         self.gamma = 0.9
-#         self.trainlog = 0
-#         self.lossfunc = nn.MSELoss()
-#         from torch.utils.tensorboard import SummaryWriter
-#         import datetime
-#         self.writer = SummaryWriter('./logdir/TargetDQN/'+datetime.datetime.now().strftime('%m%d_%H%M%S'))
-    
-#     def collectinfo(self,COUNT=128):
-#         # COUNT = 128
-#         numcollect = 0
-#         while True:
-#             done = False
-#             current_state = self.trainenv.reset()
-#             # current_state = np.array(current_state)
-#             while done == False:
-#                 if random.random() < self.epsilon:
-#                     action = random.randint(0,1)
-#                 else:
-#                     action = torch.argmax(self.net(current_state))
-#                     action = int(action)
-                
-#                 next_state,reward,done,_ = self.trainenv.step(action)
-#                 x,x_dot,theta,theta_dot = next_state
-#                 r1 = (self.trainenv.x_threshold - abs(x)) / self.trainenv.x_threshold - 0.8
-#                 r2 = (self.trainenv.theta_threshold_radians - abs(theta)) / self.trainenv.theta_threshold_radians - 0.5
-#                 reward = r1 + r2
-#                 self.memory[self.memoryindex,:] = np.hstack((current_state,[action,reward],next_state))
-#                 current_state = next_state
-#                 self.memoryindex += 1
-#                 self.memoryindex %= self.maxsize
-#                 numcollect += 1
-#                 if numcollect >= COUNT:
-#                     return
-#     def testresult(self):
-#         total_reward = 0
-#         for _ in range(self.testtimes):
-#             done = False
-#             state = self.testenv.reset()
-#             while done == False:
-#                 action = torch.argmax(self.net(state)).item()
-#                 state,reward,done,_ = self.testenv.step(action)
-#                 total_reward += reward
-#         self.testlog += 1
-#         self.writer.add_scalar('reward',total_reward/self.testtimes,self.testlog)
-#     def updatenetparameter(self):
-#         for _ in range(1):
-#             self.optimizer.zero_grad()
-#             index = np.random.choice(self.maxsize,self.batchsize)
-#             data = self.memory[index,:]
-#             current_state = data[:,:4]
-#             action = torch.from_numpy(data[:,4]).to(self.device).to(torch.int64)
-#             reward = torch.from_numpy(data[:,5]).to(self.device).to(torch.float32)
-#             next_state = data[:,6:]
-#             TDreward = reward + self.gamma * torch.max(self.targetnet(next_state),dim=-1)[0].detach()
-#             # print(self.targetnet(next_state))
-#             # print(torch.max(self.targetnet(next_state),dim=-1)[0])
-#             # exit()
-#             # print(self.net(current_state))
-#             # print(action.unsqueeze(-1))
-#             # print(torch.gather(self.net(current_state),-1,action.unsqueeze(-1)))
-#             # exit()
-#             expect_current_action_value = torch.gather(self.net(current_state),-1,action.unsqueeze(-1)).squeeze()
-            
-#             loss = self.lossfunc(expect_current_action_value,TDreward)
-            
-#             loss.backward()
-#             self.trainlog += 1
-#             self.writer.add_scalar('loss',loss,self.trainlog)
-#             self.optimizer.step()
-    
-
-#     def learn(self):
-#         # self.trainlog = 0
-#         self.collectinfo(COUNT=self.maxsize)
-#         from tqdm import tqdm
-#         train_time = 0
-#         for _ in tqdm(range(self.trainepoch)):
-#             for time in range(self.maxsize//self.batchsize):
-#                 # from time import time
-#                 # start = time()
-#                 self.collectinfo()
-#                 # end = time()
-#                 # print("Collect time is",end - start)
-#                 self.updatenetparameter()
-#                 self.testresult()
-#                 train_time += 1
-#                 # start = time()
-#                 # print('train time is',start - end)
-#                 if train_time % 64 == 0:
-#                     self.targetnet.load_state_dict(self.net.state_dict())
-#             # self.testresult()
-
-
-# if __name__ == "__main__":
-    # pass
-    # Agent = DQNAgent()
-    # Agent.learn()
+            states,actions,rewards,nextstates = self.buffer.sample(self.sample_size)
+            actions = torch.from_numpy(actions).unsqueeze(-1).to(torch.long).cuda()
+            currentQ = self.Advantagenet(states)
+            currentstateactionvalues = torch.gather(currentQ,-1,actions).squeeze()
+            nextstateactionvalues  = (torch.from_numpy(rewards).cuda() + self.gamma * torch.max(self.targetAdvantagenet(nextstates),-1)[0]).to(torch.float32).detach()
+            self.optimizer.zero_grad()
+            TDerror = self.tderrorfunction(currentstateactionvalues,nextstateactionvalues)
+            TDerror.backward()
+            self.optimizer.step()
 
 if __name__ == "__main__":
     # Agent = DQNNaive(logdir='./log/DQNNaive',shaping=True)
     # Agent = DQNTarget_softupdate(shaping=True)
-    Agent = DQNDoubleTarget_softupdate(shaping=True)
+    # Agent = DQNDoubleTarget_softupdate(shaping=True)
+    Agent = DuelingDQN(shaping=True)
     # Agent = DQNrandomsample(shaping=True)
     Agent.train()
